561f436f629b3764d347bf63d347b7d5b8640e9c
2894638479 <2894638479@qq.com>
2026-07-11 22:43:02 +0800

    alpha.18

--- src/main/java/io/github/u2894638479/kotlinmcuibackend/mixin/GameRendererMixin.java
+++ src/main/java/io/github/u2894638479/kotlinmcuibackend/mixin/GameRendererMixin.java
@@ -1,18 +1,22 @@
 package io.github.u2894638479.kotlinmcuibackend.mixin;
 
+import io.github.u2894638479.kotlinmcuibackend.DefaultBackend;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import static io.github.u2894638479.kotlinmcuibackend.DefaultBackendKt.getRenderOverlay;
-
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;flush()V"))
-    void kotlinmcuibackend$renderOverlay(GuiGraphics instance) {
-        getRenderOverlay().invoke(instance);
+    void kotlinmcuibackend$render(GuiGraphics instance) {
+        instance.pose().pushPose();
+        float f = 1 / (float)Minecraft.getInstance().getWindow().getGuiScale();
+        instance.pose().scale(f,f,1f);
+        DefaultBackend.INSTANCE.render(instance);
+        instance.pose().popPose();
         instance.flush();
     }
 }
---
+++ src/main/kotlin/io/github/u2894638479/kotlinmcuibackend/DefaultBackend.kt
@@ -0,0 +1,54 @@
+package io.github.u2894638479.kotlinmcuibackend
+
+import io.github.u2894638479.kotlinmcui.backend.*
+import io.github.u2894638479.kotlinmcui.container.DslChild
+import io.github.u2894638479.kotlinmcui.container.DslDataStore
+import io.github.u2894638479.kotlinmcui.container.topComponent
+import io.github.u2894638479.kotlinmcui.functions.DslFunction
+import io.github.u2894638479.kotlinmcui.math.px
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.screens.Screen
+import net.minecraft.network.chat.Component
+
+@InternalBackend
+object DefaultBackend : DslBackend<GuiGraphics, Screen>,
+    DslBackendRenderer<GuiGraphics> by Renderer,
+    DslBackendMetadata by Metadata,
+    DslBackendUtils by Utils,
+    DslBackendInput by Input
+{
+    private inline val mc get() = Minecraft.getInstance()
+
+    fun render(guiGraphics: GuiGraphics) {
+        context(guiGraphics, Input.mouse) {
+            topComponent.rect.apply {
+                left = 0.px
+                top = 0.px
+                right = mc.window.screenWidth.px
+                bottom = mc.window.screenHeight.px
+            }
+            topComponent.render()
+        }
+    }
+
+    private class WrappedScreen(title: String,dslFunction: DslFunction): Screen(Component.literal(title)) {
+        val parent = mc.screen
+        val dataStore = DslDataStore(
+            DefaultBackend, title,
+            { mc.execute { mc.setScreen(parent) } }, dslFunction
+        )
+        val dslScreen = dataStore.dslScreen
+        var child: DslChild? = null
+        override fun added() { child = topComponent.children.collect(dslScreen) }
+        override fun removed() { topComponent.children.remove(child!!) }
+        override fun onClose() { dslScreen.close() }
+        override fun isPauseScreen() = dataStore.pauseGame
+    }
+
+    override fun create(title:String, dslFunction: DslFunction): DslBackendScreenHolder<Screen> = object: DslBackendScreenHolder<Screen> {
+        override fun show() { mc.execute { mc.setScreen(screen) } }
+        override val screen = WrappedScreen(title,dslFunction)
+        override val dslScreen get() = screen.dslScreen
+    }
+}
